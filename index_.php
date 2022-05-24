aee0d62df925727a2e2faad1d5fa05819689ab4e
Dandelion64 <coolpillow0308@gmail.com>
2022-05-24 23:39:52 +0800

    the same as practice in class

---
+++ index_.php
@@ -0,0 +1,9 @@
+<?php require __DIR__ . '/parts/database_connect_by_pdo.php';
+$pageName= 'index_';
+$title = '首頁 - 丹德里恩的練習';
+// MVC
+?>
+<?php include __DIR__ . '/parts/html-head.php' ?>
+<?php include __DIR__ . '/parts/navbar.php' ?>
+<?php include __DIR__ . '/parts/scripts.php' ?>
+<?php include __DIR__ . '/parts/html-foot.php' ?>
